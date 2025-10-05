d1bf599c6905cd71601dc95d2bbce6ee81db78e6
smallriver0316 <stream0316@gmail.com>
2025-10-05 18:19:37 +0900

    Added settings for prettier

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +11,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'dist/**',
+      'next-env.d.ts',
+      '.eslintcache',
     ],
   },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintPluginPrettier,
 ];
 
 export default eslintConfig;
